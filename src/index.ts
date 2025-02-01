5da2080a0da1e296783738a52b28feacb9e21828
Rens Salaladaga <staleexam19@gmail.com>
2025-02-01 15:52:25 +0800

    refactor: changed folder structure

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,11 @@ import http from "http";
 import helmet from "helmet";
 import cors from "cors";
 
-import Logger from "./utils/logger";
-import { ErrorResponse } from "./lib/response";
+import Logger from "./shared/utils/logger";
+import { ErrorResponse } from "./shared/lib/response";
 import { PORT } from "./config/constants";
 
-import errorHandler from "./middlewares/errorhandler";
-
-import { hello } from "./lib/doc_parser/index.node";
+import errorHandler from "./shared/middlewares/errorhandler";
 
 export const app = express();
 export const server = http.createServer(app);
@@ -24,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // 404 Error
 app.use("*", (req, res) => {
-    let message = `Can't ${req.method} ${req.originalUrl} ${hello()}`;
+    let message = `Can't ${req.method} ${req.originalUrl}`;
     Logger.error(message);
     throw new ErrorResponse(404, message, {});
 });
