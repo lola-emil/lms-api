a83e9aa08d553078da2fd149eb50f4236c337106
Rens Saladaga <staleexam19@gmail.com>
2025-04-11 11:16:31 +0800

    feat: added academic management route

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import userManagement from "./user-management";
+import acadmicManagement from "./academic-management";
 
 const router = Router();
 
 router.use("/user-management", userManagement);
+router.use("/academic-management", acadmicManagement);
 
 export default router;
